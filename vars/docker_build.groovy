480bcc9a698384e6219da7474cde5372163837d7
devopsramesh1995 <rameshdas95.rey@gmail.com>
2026-09-21 16:59:27 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def(tring ProjectName, string ImageTag, string DockerHubUser ){
-  sh "docker built -t {$DockerHubUser}/{$ProjectName}:{$ImageTag} ."
+def(tring ImageName, string ImageTag, string DockerHubUser ){
+  sh "docker built -t {$DockerHubUser}/{$ImageName}:{$ImageTag} ."
 }
