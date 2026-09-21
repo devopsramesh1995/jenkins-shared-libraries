46515333343b7a989ae13ffe93fe387e300cfd14
devopsramesh1995 <rameshdas95.rey@gmail.com>
2026-09-21 12:15:08 +0530

    Fix echo message for successful cloning

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(string url , string branch){
   echo"this is clonnig the code"
   git url: "${url}", branch: "${branch}"
-  echo "clonning is sucessfull"
+  echo "clonning is sucessfull performed"
 }
